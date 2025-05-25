4e9fea0dae9d17e85903b80a286819248a5658ac
Andre <andrelim865@gmail.com>
2025-05-25 17:23:28 +0700

    menambah fitur display supplier bagi non-admin

--- src/main/java/com/inventa/inventory/controller/GuestController.java
+++ src/main/java/com/inventa/inventory/controller/GuestController.java
@@ -1,17 +1,36 @@
 package com.inventa.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.inventa.inventory.model.Supplier;
+import com.inventa.inventory.service.SupplierService;
 
 @Controller
 public class GuestController {
+
+    @Autowired
+    private SupplierService supplierService;
+
     @GetMapping("/")
     public String redirectToGuestItems() {
         return "guest/items";
     }
 
     @GetMapping("/suppliers")
-    public String redirectToGuestSuppliers() {
+    public String redirectToGuestSuppliers(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 7;
+        Page<Supplier> suppliersPerPage = supplierService.getSuppliersPerPage(page, pageSize);
+        
+        model.addAttribute("suppliers", suppliersPerPage.getContent());
+        model.addAttribute("currentPage", page + 1);
+        model.addAttribute("totalPages", suppliersPerPage.getTotalPages());
+        model.addAttribute("totalSuppliers", suppliersPerPage.getTotalElements());
+
         return "guest/suppliers";
     }
 }
--- src/main/java/com/inventa/inventory/controller/SupplierController.java
+++ src/main/java/com/inventa/inventory/controller/SupplierController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 @RequestMapping("/admin")
 public class SupplierController {
